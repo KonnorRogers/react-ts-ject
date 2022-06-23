7082608d8d751c28ff5c33cf63fc8935b61f2985
Konnor Rogers <konnor5456@gmail.com>
2022-06-23 15:32:15 -0400

    add notes about babel-jest

---
+++ README.md
@@ -0,0 +1,23 @@
+# Purpose
+
+React project setup with Harmony Enablers + ts-jest,
+bundled with Parcel.
+
+## Installation
+
+```bash
+npm run setup
+npm install
+```
+
+## Getting started
+
+```bash
+npm run start
+```
+
+## Testing
+
+```bash
+npm run test
+```
--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,15 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ['<rootDir>/setup-jest.js'],
   transform: {
+    // Change to "babel-jest" to use babel instead.
+    // https://github.com/facebook/jest/tree/main/packages/babel-jest#setup
     "\\.[jt]sx?$": "ts-jest",
   },
   transformIgnorePatterns: [
     "/node_modules/?!(@harmony)",
   ],
+  moduleNameMapper: {
+      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+      "\\.(s[ac]ss|css|less)$": "identity-obj-proxy"
+    }
 };
