6432f56e596d3192cbdae26ed4cee3b60ac6a3da
MSFTKonnor <108095979+MSFTKonnor@users.noreply.github.com>
2023-05-24 14:37:39 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
     "\\.[jt]sx?$": "ts-jest",
   },
   transformIgnorePatterns: [
-    "/node_modules/?!(@harmony)",
+    "/node_modules/(?!(@harmony|@lit-labs|lit|@lit|@shoelace-style))/"
   ],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
